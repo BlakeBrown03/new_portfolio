2447226a3c27e0403fc6d528e466fa103d53b789
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-05-16 14:29:01 -0500

    feat: add personal image to Home component
    - Included an image of the author in the Home component to enhance personal branding.
    - Adjusted image styling for a rounded appearance and responsive sizing.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ import AboutMe from "./about-me";
 export default function Home() {
 	return (
 		<>
+			<img
+				src="/images/me.jpg"
+				alt="Picture of Me"
+				className="w-1/2 h-1/2 rounded-full"
+			/>
 			<NavBar />
 			<AboutMe />
 		</>
