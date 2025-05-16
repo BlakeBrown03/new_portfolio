1ffd58ab75c98146e5af290b32e2016f2ea90843
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-05-16 17:01:04 -0500

    feat: update Home component with new personal image
    - Replaced the previous image with a new personal image in the Home component to enhance visual branding.
    - Adjusted image styling for improved dimensions and layout, ensuring a more cohesive presentation alongside the author's name.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,15 @@ import AboutMe from "./about-me";
 export default function Home() {
 	return (
 		<>
-			<img
-				src="/images/me.jpg"
-				alt="Picture of Me"
-				className="w-1/2 h-1/2 rounded-full"
-			/>
 			<NavBar />
+			<div className="flex flex-row items-center justify-center gap-8 my-40">
+				<img
+					src="/images/picture-of-me.jpg"
+					alt="Picture of Me"
+					className="w-128 h-128 rounded-full object-cover"
+				/>
+				<h1 className="text-4xl font-bold">Blake Brown</h1>
+			</div>
 			<AboutMe />
 		</>
 	);
