79d68dbfffa64870d17a550840710d8af4012bdf
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-05-17 11:09:23 -0500

    feat: enhance Home component with animated title
    - Updated the Home component to include an animated title using ReactTyped, showcasing the author's name and profession.
    - Adjusted layout to a grid format for better alignment and presentation of the personal image and title.
    - Improved styling for a more cohesive and visually appealing user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,35 @@
+"use client";
+
 import NavBar from "@/components/ui/nav-bar";
 import AboutMe from "./about-me";
+import { ReactTyped } from "react-typed";
 
 export default function Home() {
 	return (
 		<>
 			<NavBar />
-			<div className="flex flex-row items-center justify-center gap-8 my-40">
-				<img
-					src="/images/picture-of-me.jpg"
-					alt="Picture of Me"
-					className="w-128 h-128 rounded-full object-cover"
-				/>
-				<h1 className="text-4xl font-bold">Blake Brown</h1>
+			<div className="grid grid-cols-2 items-center justify-items-center my-32">
+				<div className="flex justify-center ml-10">
+					<img
+						src="/images/picture-of-me.jpg"
+						alt="Picture of Me"
+						className="w-128 h-128 rounded-full object-cover"
+					/>
+				</div>
+				<div className="flex justify-center">
+					<ReactTyped
+						className="text-5xl font-bold"
+						strings={[
+							"Blake Brown",
+							"Software Engineer",
+							"Web Developer"
+						]}
+						startDelay={1000}
+						typeSpeed={100}
+						backSpeed={100}
+						loop
+					/>
+				</div>
 			</div>
 			<AboutMe />
 		</>
