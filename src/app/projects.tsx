96ce0f677eb2ce582ec227d307ff12d5c082e45e
BlakeBrown03 <blake.m.brown03@gmail.com>
2025-05-20 21:50:34 -0500

    Making projects a carousel instead of a grid. Feels nice to view one project at a time.

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -1,3 +1,10 @@
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious
+} from "@/components/ui/carousel";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,14 +14,14 @@ export default function Projects() {
 			id: 1,
 			name: "Meraki API",
 			description: "A REST API for the Meraki platform",
-			image: "/meraki-api.png",
+			image: "/Meraki.png",
 			link: "https://meraki-api.com"
 		},
 		{
 			id: 2,
 			name: "CSV to JSON Converter",
-			description: "A simple CSV to JSON converter",
-			image: "/csv-to-json.png",
+			description: "A CLI interface for CSV to JSON conversion",
+			image: "/Csv.png",
 			link: "https://csv-to-json.com"
 		},
 		{
@@ -30,29 +37,23 @@ export default function Projects() {
 	return (
 		<div className="flex flex-col items-center justify-center my-4">
 			<h1 className="text-4xl font-bold my-4">Projects</h1>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-				{projects.map(project => (
-					<div
-						key={project.id}
-						className="flex flex-col items-center">
-						<h2 className="text-2xl font-bold">{project.name}</h2>
-						<p className="text-sm text-gray-500">
-							{project.description}
-						</p>
-						<Link
-							href={project.link}
-							className="text-sm text-gray-500">
-							View Project
-						</Link>
-						<Image
-							src={project.image}
-							alt={project.name}
-							width={100}
-							height={100}
-						/>
-					</div>
-				))}
-			</div>
+			<Carousel>
+				<CarouselContent>
+					{projects.map(project => (
+						<CarouselItem key={project.id}>
+							<Image
+								src={project.image}
+								alt={project.name}
+								width={300}
+								height={300}
+								className="rounded-lg"
+							/>
+						</CarouselItem>
+					))}
+				</CarouselContent>
+				<CarouselPrevious />
+				<CarouselNext />
+			</Carousel>
 		</div>
 	);
 }
